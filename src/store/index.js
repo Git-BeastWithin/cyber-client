1d6cc13c7183228d446fdb5cb87c80ef70ed07fa
beastwithin <beastwithin0@qq.com>
2023-02-01 11:07:39 +0800

    完成了项目起步

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,16 @@
 import { createStore } from 'vuex'
+import createPersistedstate from 'vuex-persistedstate'
+
+import user from './modules/user'
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    user
+  },
+  plugins: [
+    createPersistedstate({
+      key: 'cyber-client-store',
+      paths: ['user']
+    })
+  ]
 })
